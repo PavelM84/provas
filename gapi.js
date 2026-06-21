0da8d93968cc991f75d8674e0865a907607ed966
PavelM84 <pavel.a.melnikov@gmail.com>
2026-06-21 16:18:40 +0400

    Delete gapi.js

--- gapi.js
+++
@@ -1,8 +0,0 @@
-function initClient() {
-  gapi.client.init({
-    apiKey: "AIzaSyDWZGfiG3f55Zpe0K3kkjoTcZzvMAONKb4",
-    discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
-  }).then(loadData);
-}
-
-gapi.load("client", initClient);
